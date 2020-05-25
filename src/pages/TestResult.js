bd986a40be31193cfff9d2f3320b91a974a7e68d
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-25 13:05:15 +0200

    Add FAB in TestResult page - click the FAB to get back to the Dashboard

--- src/pages/TestResult.js
+++ src/pages/TestResult.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useHistory, useLocation } from "react-router-dom";
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Fab } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import ResultTable from '../components/ResultTable';
 
@@ -20,18 +20,38 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
         padding: theme.spacing(2),
     },
+    fabContainer: {
+        flexGrow: 1,
+        position: 'fixed',
+        top: theme.spacing(1.5),
+        right: theme.spacing(2),
+    },
 }));
 
 const TestResult = () => {
     const location = useLocation();
     const classes = useStyles();
+    const history = useHistory();
     const testResultData = [...location.state.testResultData];
     console.log(`inside test result: ${location.state.testResultData}`);
+    const onDashboardClick = () => {
+        history.push('/');
+    }
     return(
-        <Grid container spacing={3} justify={"center"}>
+        <Fragment>
+                <Fab variant="extended"
+                    size="medium"
+                    color="secondary"
+                    aria-label="back"
+                    onClick={onDashboardClick}
+                    className={classes.fabContainer}
+                >
+                    Dashboard
+                </Fab>
+            <Grid container spacing={3} justify={"center"}>
             <Grid item sm={12} md={12} xs={12}>
                 <Typography variant="h4" color={'primary'} className={classes.resultTitle} >
-                    Your Score
+                    You scored
                 </Typography>
                 <Typography variant="h3" color={'secondary'} className={classes.resultTitle} >
                     {location.state.testScore}%
@@ -39,6 +59,7 @@ const TestResult = () => {
             </Grid>
             <ResultTable testResultData={testResultData} />
         </Grid>
+        </Fragment>
     );
 };
 
