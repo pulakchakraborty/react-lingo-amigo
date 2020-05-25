4dba642cc509c002410c3fdddf329a9ce5d31eeb
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-05-25 02:34:01 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const Root = () => {
+    return(
+        <Fragment>
+            <App />
+        </Fragment>
+    );
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
