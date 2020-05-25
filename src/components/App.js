1c7c007e40ecbfab24644519b7b52d3714dd3cc9
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-25 11:43:45 +0200

    manage wordBank state on app level - better state management

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Header from './Header';
@@ -20,11 +20,50 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
     const classes = useStyles();
+    const history = useHistory();
+    const [ wordBank, setWordBank ] = useState([]);
+
+    /* This function adds a new pair of words in the word-list */
+    const growWordBank = ({ english, german }) => {
+        console.log(`data Dashboard: ${english} : ${german}`);
+        setWordBank([...wordBank, { english, german }]);
+        console.log(`data Dashboard: ${wordBank}`);
+    };
+
+    /* This function removes a specific pair of words from the word-list */
+    const deleteWord = (index) => {
+        const temp = [...wordBank];
+        temp.splice(index, 1);
+        setWordBank(temp);
+    };
+
+    /* This function handles the randomization of wordlist and redirecting
+        the user to /language-test route when the test starts
+    */
+    const onStartTest = () => {
+        if (wordBank.length > 19)   // Change the hardcoded value in future
+        {
+            let shuffledWordBank = [...wordBank]
+                .sort(() => 0.5 - Math.random())
+                .slice(0, 20);      // change the hardcoded value in future
+            history.push('/language-test', { selectedWordBank: [...shuffledWordBank] });
+        }
+    };
+
     return(
         <div className={classes.appContainer}>
             <Header />
             <Switch>
-                <Route exact path="/dashboard" component={Dashboard}/>
+                <Route exact path="/dashboard"
+                        render={(props) =>
+                            <Dashboard {...props}
+                                wordBank={wordBank}
+                                growWordBank={growWordBank}
+                                deleteWord={deleteWord}
+                                onStartTest={onStartTest}
+                            />
+                        }
+                />
                 <Route exact path="/">
                     <Redirect to="/dashboard" />
                 </Route>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,4 @@
-import React, { Fragment, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { Fragment } from 'react';
 import Fab from '@material-ui/core/Fab';
 import EditIcon from '@material-ui/icons/Edit';
 import { Container } from '@material-ui/core';
@@ -26,37 +25,8 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const Dashboard = () => {
+const Dashboard = ({ wordBank, growWordBank, deleteWord, onStartTest }) => {
     const classes = useStyles();
-    const history = useHistory();
-    const [ wordBank, setWordBank ] = useState([]);
-
-    /* This function adds a new pair of words in the word-list */
-    const growWordBank = ({ english, german }) => {
-        console.log(`data Dashboard: ${english} : ${german}`);
-        setWordBank([...wordBank, { english, german }]);
-        console.log(`data Dashboard: ${wordBank}`);
-    };
-
-    /* This function removes a specific pair of words from the word-list */
-    const deleteWord = (index) => {
-        const temp = [...wordBank];
-        temp.splice(index, 1);
-        setWordBank(temp);
-    };
-
-    /* This function handles the randomization of wordlist and redirecting
-        the user to /language-test route when the test starts
-    */
-    const onStartTest = () => {
-        if (wordBank.length > 19)   // Change the hardcoded value in future
-        {
-            let shuffledWordBank = [...wordBank]
-                .sort(() => 0.5 - Math.random())
-                .slice(0, 20);      // change the hardcoded value in future
-            history.push('/language-test', { selectedWordBank: [...shuffledWordBank] });
-        }
-    };
 
     return(
         <Fragment>
